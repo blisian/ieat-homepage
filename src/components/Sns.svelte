37f4679ccb258dae58982935748c7e4dae59f54e
blisian <wlghks1599@gmail.com>
2024-11-07 15:14:09 +0900

    add instagram widget

--- src/components/Sns.svelte
+++ src/components/Sns.svelte
@@ -1,12 +1,15 @@
+
 <script>
 	import SectionWrapper from "./SectionWrapper.svelte";
 
 </script>
+
 <SectionWrapper>
     <div class="max-w-[800px] mx-auto w-full
     items-center justify-between py-6 pt-10">
-        <div class=" justify-center w-full flex text-2xl">
+        <div class=" justify-center w-full flex text-2xl mb-4">
             <h1 class=" text-2xl">I-EAT 인스타그램 둘러보기</h1>
         </div>
+        <iframe title="instagram" src="https://widget.tagembed.com/2136386" style="width:100%;height:600px;border:none;"></iframe>
     </div>
 </SectionWrapper>
